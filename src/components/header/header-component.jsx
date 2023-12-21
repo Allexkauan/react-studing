9ba6f2481046abb75725703480a68622cea1eb4a
Allexkauan <alexpaivalima@hotmail.com>
2023-12-21 17:23:43 -0300

    react app

---
+++ src/components/header/header-component.jsx
@@ -0,0 +1,13 @@
+import HeaderContent from './style';
+
+
+const Header = () => {
+ 
+  return (
+    <>
+    <HeaderContent><p>Hello world</p></HeaderContent>
+    </>
+  )
+}
+
+export default Header
